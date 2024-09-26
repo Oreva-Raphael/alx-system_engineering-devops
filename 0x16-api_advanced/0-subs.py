9e508baee5337421592e50f72a8fe5c7650c536b
Orevaoghene Eguwe <orevaoghene.raphael@gmail.com>
2024-09-26 18:55:40 +0100

    coding style conformity

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,13 +5,14 @@ request and use data from reddit
 
 from requests import get
 
+
 def number_of_subscribers(subreddit):
     """
     A function to query Reddit API and return the number of subscribers
     """
     if not (subreddit and isinstance(subreddit, str)):
         return 0
-    
+
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     user_agent = {'User-Agent': 'Google Chrome Version 129.0.6668.70'}
     response = get(url, headers=user_agent)
@@ -20,4 +21,4 @@ def number_of_subscribers(subreddit):
     try:
         return results.get('data').get('subscribers')
     except Exception:
-        return 0  
+        return 0
